ee03d33129c142fff571269a3663e66b1c505a65
ratovia <ratovia@hotmail.co.jp>
2021-04-01 03:31:41 +0900

    change index.js functional component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const index = () => {
+  return (
+    <div>
+      hello world
+    </div>
+  )
 }
+
+export default index
