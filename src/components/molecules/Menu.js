c457d8aa56f129aebe8c331bf4dd38f9c4c8085a
ratovia <ratovia@hotmail.co.jp>
2021-04-06 09:34:17 +0900

    add  nav link

--- src/components/molecules/Menu.js
+++ src/components/molecules/Menu.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { FrederickaH3 } from "../atoms/FrederickaText"
-
+import { Link } from "gatsby"
 const Flex = styled.ul`
   display: flex;
   flex-direction: row;
@@ -14,21 +14,34 @@ const Flex = styled.ul`
   align-items: center;
 `
 
+const NavLink = styled(Link)`
+  color: white;
+  color: white;
+  text-decoration: none;
+`
 export const Menu = () => {
   return (
     <Flex>
-      <li>
-        <FrederickaH3 label="Top" />
-      </li>
-      <li>
-        <FrederickaH3 label="About" />
-      </li>
-      <li>
-        <FrederickaH3 label="Work" />
-      </li>
-      <li>
-        <FrederickaH3 label="Links" />
-      </li>
+      <NavLink to="/">
+        <li>
+          <FrederickaH3 label="Top" />
+        </li>
+      </NavLink>
+      <NavLink to="/about">
+        <li>
+          <FrederickaH3 label="About" />
+        </li>
+      </NavLink>
+      <NavLink to="/work">
+        <li>
+          <FrederickaH3 label="Work" />
+        </li>
+      </NavLink>
+      <NavLink to="/links">
+        <li>
+          <FrederickaH3 label="Links" />
+        </li>
+      </NavLink>
     </Flex>
   )
 }
