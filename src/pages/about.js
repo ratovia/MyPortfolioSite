58448874429e279ff259dc110ce244e6d50508c7
ratovia <ratovia@hotmail.co.jp>
2021-04-06 09:02:57 +0900

    about  work page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { MessageArea } from "../components/organisms/MessageArea"
 import MyProfile from "../../static/MyProfile.jpg"
-
 import { Navigation } from "../components/organisms/Navigation"
 import { Footer } from "../components/organisms/Footer"
 import { ContentTemplate } from "../components/templates/ContentTemplate"
+import { FlowList } from "../components/organisms/FlowList"
 
 const about = () => {
   return (
@@ -18,7 +18,39 @@ const about = () => {
 得意のRailsや周辺知識を生かしながらOSSコントリビューターになりたい"
         ></MessageArea>
       }
-      mainContent={<div></div>}
+      mainContent={
+        <FlowList
+          flows={[
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社xxx",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社yyy",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社zzz",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+          ]}
+        />
+      }
       footerContent={<Footer></Footer>}
     ></ContentTemplate>
   )
--- src/pages/work.js
+++ src/pages/work.js
@@ -4,13 +4,46 @@ import { Footer } from "../components/organisms/Footer"
 import { ContentTemplate } from "../components/templates/ContentTemplate"
 import bgImage from "../../static/bg.jpg"
 import { HeroImage } from "../components/organisms/HeroImage"
+import { FlowList } from "../components/organisms/FlowList"
 
 const work = () => {
   return (
     <ContentTemplate
       navigation={<Navigation></Navigation>}
       heroContent={<HeroImage imageUrl={bgImage}></HeroImage>}
-      mainContent={<div></div>}
+      mainContent={
+        <FlowList
+          flows={[
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社xxx",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社yyy",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+            {
+              startDate: "2019年8月",
+              endDate: "現在",
+              orgName: "株式会社zzz",
+              period: "1年間",
+              content:
+                "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld",
+              batchs: ["営業", "エンジニア", "セールス", "労務"],
+            },
+          ]}
+        />
+      }
       footerContent={<Footer></Footer>}
     ></ContentTemplate>
   )
